a5187acc02fb4f9e8cb45d307c7cadbd6da19b2f
zx <2540112079@qq.com>
2021-07-23 13:44:17 +0800

    这是我的第一个student类，它只有两个属性

---
+++ TestModule01/src/com/zx/Student.java
@@ -0,0 +1,12 @@
+package com.zx;
+
+/**
+ * @Auther: zhangxin
+ * @Date: 2021/7/23 - 07 - 23 - 13:41
+ * @Description: com.zx
+ * @version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
