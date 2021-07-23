289959ed98d78c7d6e6e1ac9999214cab08efa16
zx <2540112079@qq.com>
2021-07-23 14:29:14 +0800

    新增了一个People类，只有一个属性

---
+++ TestModule01/src/com/zx/People.java
@@ -0,0 +1,11 @@
+package com.zx;
+
+/**
+ * @Auther: zhangxin
+ * @Date: 2021/7/23 - 07 - 23 - 14:27
+ * @Description: com.zx
+ * @version: 1.0
+ */
+public class People {
+    private String name;
+}
